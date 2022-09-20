f3426ab416900abff91fe51c4c8abf931381501a
Aarti Rade <aartirade@gmail.com>
2022-09-20 18:20:00 +0530

    GFG

---
+++ BST/Lowest-Common-Ancestor-in-a-BST.py
@@ -0,0 +1,7 @@
+def LCA(root, n1, n2):
+    if n1<root.data and n2<root.data:
+        return LCA(root.left,n1,n2)
+    elif n1>root.data and n2>root.data:
+        return LCA(root.right,n1,n2)
+    else:
+        return root
